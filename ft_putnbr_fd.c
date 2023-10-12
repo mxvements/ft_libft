670b4125a302c406955b69d0247b8801254d2c38
Lucia Martin De Aguilera Mielgo <luciama2@c2r4s1.42madrid.com>
2023-10-12 19:24:59 +0200

    ft_itoa.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,35 +6,38 @@
 /*   By: luciama2 <luciama2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 14:00:27 by luciama2          #+#    #+#             */
-/*   Updated: 2023/10/01 16:49:49 by luciama2         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:27:56 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 /* ft_putnbr_fd
- * LIB                  -
- * PROTOTYPE            void    ft_putnbr_fd(int n, int fd);
- * PARAMETERS           n: the integer to output
+ * lib                  -
+ * prototype            void    ft_putnbr_fd(int n, int fd);
+ * parameters           n: the integer to output
  *                      fd: the file descriptor on which to write
- * RETURN               none
- * EXT FUNCTS           write <unistd.h>
- * DESCRIPTION          outputs the integer 'n' to the given file descriptor 
- * */
+ * return               none
+ * ext functs           write
+ * description          outputs the integer 'n' to the given file descriptor 
+*/
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void    ft_putnbr_fd(int n, int fd)
 {
-	char		c;
-	long long	nl;
+    char       c;
+    long long   nl;
 
-	if (n < 0)
-	{
-		n *= -1;
-		write(fd, "-", 1);
-	}
-	nl = (long) n;
-	if (nl >= 10)
-		ft_putnbr_fd((int)nl / 10, fd);
-	c = (nl % 10) + '0';
-	write(fd, &c, 1);
+    if (n < 0)
+    {
+        n *= -1;
+        write(fd, "-", 1);
+    }
+    nl = (long) n;
+    if (nl >= 10)
+    {
+        ft_putnbr_fd((int)nl / 10, fd);
+    }
+    c = (nl % 10)  + '0';
+    write(fd, &c, 1);
 }
