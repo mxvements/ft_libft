341f1e06c567e584617079e7e730157ec52d6bf6
mxvements <lmmielgo@DESKTOP-57O0OKS>
2023-10-15 22:24:30 +0200

    ft_lstmap.c

--- Makefile
+++ Makefile
@@ -50,7 +50,8 @@ MY_BONUS_SOURCES=	ft_lstnew.c \
 					ft_lstadd_back.c \
 					ft_lstdelone.c \
 					ft_lstclear.c \
-					ft_lstiter.c
+					ft_lstiter.c \
+					ft_lstmap.c
 
 MY_OBJECTS=$(MY_SOURCES:%.c=%.o)	
 
---
+++ ft_lstmap.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciama2 <luciama2@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 21:20:24 by luciama2          #+#    #+#             */
+/*   Updated: 2023/10/15 21:20:28 by luciama2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ft_lstmap.c		bonus
+ * LIB				-
+ * PROTOTYPE		t_list *ft_lstmap(t_list *lst, 
+ * 									  void *(*f)(void *), void (*del)(void *))
+ * TURN IN FILES	-
+ * PARAMETERS		lst: address of a ptr to a node
+ * 					f: address of a funct to iterate on the list
+ * 					del: address of a funct to delete the content of a node, 
+ * 					if needed		
+ * RETURN VALUE		t_list, the new list
+ * 					NULL if the allocation fails
+ * EXTERNAL FUNCTS	malloc, free
+ * DESCRIPTION		iterates the list 'lst' and applies the funcion 'f' on the
+ * 					content of each node.
+ * 					creates a new list resulting of the successive applications
+ * 					of the function 'f'
+ * 					the 'del' function is used to delete the content of a node
+ * 					if needed
+*/
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newlst;
+	t_list	*node;
+	t_list	*cpy;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	cpy = lst;
+	newlst = NULL;
+	while(cpy)
+	{
+		node = ft_lstnew(f(cpy->content));
+		if (!node)
+			ft_lstclear(&newlst, del);
+		ft_lstadd_back(&newlst, node);
+		cpy = cpy->next;
+	}
+	return (newlst);
+}
