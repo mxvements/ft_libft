6a32716b69ba216bc66aa441f67517d6d3a92c3b
Lucia Martin De Aguilera Mielgo <luciama2@c2r4s1.42madrid.com>
2023-10-15 16:03:17 +0200

    ft_lstlast.c

--- Makefile
+++ Makefile
@@ -45,7 +45,8 @@ MY_SOURCES= ft_islower.c \
 
 MY_BONUS_SOURCES=	ft_lstnew.c \
 					ft_lstadd_front.c \
-					ft_lstsize.c
+					ft_lstsize.c \
+					ft_lstlast.c
 
 MY_OBJECTS=$(MY_SOURCES:%.c=%.o)	
 
---
+++ ft_lstlast.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciama2 <luciama2@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 15:55:33 by luciama2          #+#    #+#             */
+/*   Updated: 2023/10/15 15:58:03 by luciama2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ft_lstlast			bonus
+ * LIB					-
+ * PROTOTYPE			t_list ft_llstlast(t_list *lst)
+ * TURN IN FILES		-
+ * PARAMETERS			lst: the beginning of the list
+ * RETURN VALUE			t_list, last node of the list
+ * EXTERNAL FUNCTS		none
+ * DESCRIPTION			return the last node of the list
+ */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*last;
+
+	last = lst;
+	if (!last)
+		return (NULL);
+	while (last->next != NULL)
+		last = last->next;
+	return (last);
+}
